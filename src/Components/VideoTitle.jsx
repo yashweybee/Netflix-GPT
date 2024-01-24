646d0c5c7385fda8b50d0d6cede2e7dde38b98d5
yashweybee <yashparmar@weybee.in>
2024-01-24 10:08:54 +0530

    -mainContainer completed

--- src/Components/VideoTitle.jsx
+++ src/Components/VideoTitle.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-64 pl-24">
-      <h1 className="font-bold text-6xl">{title}</h1>
-      <p className="text-lg py-6 w-1/2">{overview}</p>
+    <div className="w-screen aspect-video pt-[20%] pl-24 absolute text-white bg-gradient-to-r  from-black">
+      <h1 className="font-bold text-7xl">{title}</h1>
+      <p className="text-xl py-6 w-1/4">{overview}</p>
       <div className="">
-        <button className=" bg-gray-700 text-black text-lg p-3 w-32 rounded ">
+        <button className="bg-white text-black text-xl p-3 w-32 mx-2 rounded">
           Play
         </button>
-        <button className="bg-gray-400 text-black text-lg p-3 w-32 mx-2 rounded">
+        <button className="bg-gray-500 text-white bg-opacity-50 text-xl p-3 w-32 mx-2 rounded">
           More Info
         </button>
       </div>
